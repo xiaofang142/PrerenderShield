24c60cf8e894ecd2e4cd243d6943ff61d3930686
xiaofang <>
2026-01-08 09:47:57 +0800

    Update 2026-01-08 09:47:57

---
+++ web/src/pages/Logs/Logs.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Logs: React.FC = () => {
+  return (
+    <div>
+      <h1 className="page-title">日志管理</h1>
+      <div className="container">
+        <p>日志管理页面</p>
+      </div>
+    </div>
+  )
+}
+
+export default Logs
